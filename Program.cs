68acecbb73162bc2b2ba9778b4a1798ea481ab65
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-14 10:53:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignments_2._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Design a class hierarchy of your choice.
+
+            //Sports
+            //Team Sports
+            //Basketball -- Soccer -- Volleyball
+            //3 Men Basketball -- 5 Men indoor soccer -- Beach Volleyball        
+        }
+    }
+}
